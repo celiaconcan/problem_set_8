2132035bed369c55dc49286e49e5503e512fde22
Tate Green <tategreen@dhcp-10-250-165-184.harvard.edu>
2019-04-18 15:39:03 -0400

    commit

--- Shotspotter.R
+++ Shotspotter.R
@@ -8,7 +8,20 @@ library(stringr)
 library(ggthemes)
 library(tidyverse)
 
-oakland <- read_csv("http://justicetechlab.org/wp-content/uploads/2017/08/OakShots_latlong.csv")
+oakland <- read_csv("http://justicetechlab.org/wp-content/uploads/2017/08/OakShots_latlong.csv",
+                    col_types = cols(
+                      OBJECTID = col_double(),
+                      CAD_ = col_character(),
+                      BEAT = col_character(),
+                      DATE___TIM = col_character(),
+                      ADDRESS = col_character(),
+                      CALL_ID = col_character(),
+                      DESCRIPTIO = col_character(),
+                      Xrough = col_double(),
+                      Yrough = col_double(),
+                      XCOORD = col_double(),
+                      YCOORD = col_double()
+                    ))
 
 
 shapes <- urban_areas(class = "sf") 
@@ -17,10 +30,10 @@ shapes <- urban_areas(class = "sf")
 shapes2 <- shapes %>%
 filter(NAME10 == "San Francisco--Oakland, CA")
 
-x <- st_as_sf(shapes2, coords = c("XCOORD", "YCOORD"), crs = 4236)
+oakland <- st_as_sf(oakland, coords = c("XCOORD", "YCOORD"), crs = 4236)
 
-ggplot()+ 
+ggplot(data = shapes2)+ 
   geom_sf() +
-  geom_sf(data = shapes2) + 
+  geom_sf(data = oakland) + 
   theme_map()
 
